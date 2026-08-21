0b47a1a1b2558425c0275c1ee4c79cff80e2142d
mrsantichai-del <mr.santichai@gmail.com>
2026-08-21 08:32:05 +0700

    Fix location type select not populated on edit

--- web-ui/src/features/master-locations/MasterLocationForm.tsx
+++ web-ui/src/features/master-locations/MasterLocationForm.tsx
@@ -35,7 +35,13 @@ export const MasterLocationForm: React.FC<Props> = ({ initialData, onSubmit, isL
   const fetchLocatypes = async () => {
     try {
       const res = await fetch('/api/master-locatypes', { headers: {  }});
-      if (res.ok) setLocatypes(await res.json());
+      if (res.ok) {
+        const data = await res.json();
+        setLocatypes(data);
+        if (initialData?.location_locatype) {
+          setValue('location_locatype', initialData.location_locatype);
+        }
+      }
     } catch (e) {
       console.error('Failed to fetch locatypes', e);
     }
